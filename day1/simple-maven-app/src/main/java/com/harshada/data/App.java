6909e33458fec7af205c85d729bfd64efe58f40c
admin <harshada@email.com>
2023-08-31 23:51:22 -0700

    commited changes

---
+++ day1/simple-maven-app/src/main/java/com/harshada/data/App.java
@@ -0,0 +1,13 @@
+package com.harshada.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Harshada here." );
+    }
+}
