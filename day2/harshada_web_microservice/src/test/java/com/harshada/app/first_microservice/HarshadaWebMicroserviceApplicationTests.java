6ffe0171a837485d87893e90494af8ffcc849468
admin <harshada@email.com>
2023-09-03 23:09:46 -0700

    first microservice

---
+++ day2/harshada_web_microservice/src/test/java/com/harshada/app/first_microservice/HarshadaWebMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshada.app.first_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarshadaWebMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
