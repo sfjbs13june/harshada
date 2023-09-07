13b39e50415cc7a51ffa3b7a516c818d6ba928df
admin <harshada@email.com>
2023-09-07 00:46:39 -0700

    spring student app

---
+++ day06/spring-student-app/src/test/java/com/harshada/app/SpringStudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshada.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
